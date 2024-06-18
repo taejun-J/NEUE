15a3ef29cdc792487a8aac4b85061cb73666dedb
taejun-J <sagecandy20@gmail.com>
2024-06-18 20:58:08 +0900

    nav추가 및 vision수정

--- vision_script.js
+++ vision_script.js
@@ -7,18 +7,26 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  introVideo.hide(); // 캔버스에 영상을 직접 표시하지 않음
-  introVideo.size(1080, 1920);
+  introVideo.hide();
   introVideo.volume(0);
 }
 
 function draw() {
   background(0);
   //정렬
-  let x = (width - introVideo.width) / 2;
-  let y = (height - introVideo.height) / 2;
-  image(introVideo, x, y);
+  let introVideowidth = 1030;
+  let introVideoheight = 1300;
+  let x = (width - introVideowidth) / 2;
+  let y = (height - introVideoheight) / 2;
+  image(introVideo, x, y, introVideowidth, introVideoheight);
   if (introEnded) {
+    // fill(0);
+    // rect(
+    //   (width - introVideo.width) / 2,
+    //   (height - introVideo.height) / 2,
+    //   introVideo.width,
+    //   introVideo.height
+    // );
     document.getElementById('main-content').style.display = 'block';
   }
 }
